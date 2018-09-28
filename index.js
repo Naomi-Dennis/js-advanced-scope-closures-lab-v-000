7d74141f8cb2a7af728d6f3c1a2db6b1221a4e20
Naomi Dennis <njdennis94@gmail.com>
2018-09-28 18:49:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 function produceDrivingRange(blockRange){
   return (start, finish) => { 
+  
     start = start.split()
     finish = finish.split()
     start.pop()
     start.pop()
     finish.pop()
     finish.pop()
+ 
+    
     start = Number(start)
     finish = Number(finish)
-    return (finish - start <= blockRange)
+    
+       
+    let dist = finish - start 
+    
+    return (dist <= blockRange ? dist + " out of range" : 0)
     
   }
 }
