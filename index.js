57e92a25f0db6029951f640a5df1ff553ae2e2ef
Naomi Dennis <njdennis94@gmail.com>
2018-09-28 18:47:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function produceDrivingRange(blockRange){
-  return () => { }
+  return (start, finish) => { 
+    start.pop()
+    start.pop()
+    finish.pop()
+    finish.pop()
+    start = Number(start)
+    finish = Number(finish)
+    return (finish - start <= blockRange)
+    
+  }
 }
