ab591e956bd1f4cdcebfd718af88cb850628df64
Naomi Dennis <njdennis94@gmail.com>
2018-09-28 18:55:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function produceDrivingRange(blockRange){
     let dist = Math.abs(finish - start)
     let range = dist - blockRange
 
-    return (range > 0 ? range + " blocks out of range" : Math.abs(range) + " within range")
+    return (range > 0 ? range + " blocks out of range" : "within range by " + Math.abs(range))
     
   }
 }
